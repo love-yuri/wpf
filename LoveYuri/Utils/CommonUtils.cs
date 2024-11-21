2aa687fa03189fc0bcfe8d6d9b8991484f267b43
love-yuri <yuri2078170658@gmail.com>
2024-11-21 16:46:03 +0800

    feat(Utils): 添加CommonUtils工具类及实例创建和枚举转换方法

---
+++ LoveYuri/Utils/CommonUtils.cs
@@ -0,0 +1,37 @@
+﻿using LoveYuri.BaseException;
+
+namespace LoveYuri.Utils;
+
+/// <summary>
+/// 基础工具
+/// </summary>
+public static class CommonUtils {
+    /// <summary>
+    /// 根据泛型创造一个实例
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public static T CreateInstance<T>() where T : class {
+        var type = typeof(T);
+        var instance = Activator.CreateInstance(type);
+        if (instance == null) throw new Exception($"{type.Name} 实例创建失败!!!");
+        return instance as T ?? throw new TypeCastException(type);
+    }
+    
+    /// <summary>
+    /// int 转枚举，如果失败则抛异常
+    /// </summary>
+    /// <param name="value">枚举int值</param>
+    /// <typeparam name="T">枚举类型</typeparam>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public static T IntToEnum<T> (int value) where T : Enum {
+        if (Enum.IsDefined(typeof(T), value)) {
+            // 如果是有效的枚举值
+            return (T)(object)value;
+        }
+
+        throw new EnumCaseException($"无效的枚举值: {value}");
+    }
+}

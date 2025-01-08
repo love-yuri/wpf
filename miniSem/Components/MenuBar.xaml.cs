7d89c58a52967a7d328dd81810dbd6da70de0c5c
love-yuri <yuri2078170658@gmail.com>
2025-01-08 15:18:29 +0800

    feat(MenuBar): 增加语言切换和主题切换功能

--- miniSem/Components/MenuBar.xaml.cs
+++ miniSem/Components/MenuBar.xaml.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Input;
 using miniSem.Utils;
 
 namespace miniSem.Components {
+    public delegate void MenubarClickEvent();
+    
     public partial class MenuBar {
+        public MenubarClickEvent ImportImgClick = null;
+        private LanguageType _languageType = LanguageType.Chinese;
+        private ThemeType _themeType = ThemeType.Light;
         
         public MenuBar() {
             InitializeComponent();
@@ -24,6 +27,52 @@ namespace miniSem.Components {
 
         private void MiniWindow(object sender, RoutedEventArgs e) {
             CommonUtils.CurrentWindow.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 切换语言
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChangeLanguage(object sender, RoutedEventArgs e) {
+            switch (_languageType) {
+                case LanguageType.English: {
+                    ResourceUtils.ChangeLanguage(LanguageType.Chinese);
+                    _languageType = LanguageType.Chinese;
+                    break;
+                }
+                case LanguageType.Chinese: {
+                    ResourceUtils.ChangeLanguage(LanguageType.English);
+                    _languageType = LanguageType.English;
+                    break;
+                }
+                default: {
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 切换主题
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChangeTheme(object sender, RoutedEventArgs e) {
+            switch (_themeType) {
+                case ThemeType.Dark: {
+                    ResourceUtils.ChangeTheme(ThemeType.Light);
+                    _themeType = ThemeType.Light;
+                    break;
+                }
+                case ThemeType.Light: {
+                    ResourceUtils.ChangeTheme(ThemeType.Dark);
+                    _themeType = ThemeType.Dark;
+                    break;
+                }
+                default: {
+                    return;
+                }
+            }
         }
     }
 }

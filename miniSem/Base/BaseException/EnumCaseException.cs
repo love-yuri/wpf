ccd1919f3788590bbe7a606c221366348c4a169e
love-yuri <yuri2078170658@gmail.com>
2025-01-10 15:29:51 +0800

    feat(BaseException): 添加EnumCaseException和TypeCastException类

---
+++ miniSem/Base/BaseException/EnumCaseException.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable UnusedType.Global
+namespace miniSem.Base.BaseException {
+
+    /// <summary>
+    /// 用户处理枚举转换失败的错误
+    /// </summary>
+    public class EnumCaseException : System.Exception {
+        public EnumCaseException() : base("枚举类型转换失败!!") { }
+        public EnumCaseException(string message) : base(message) { }
+        public EnumCaseException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
---
+++ miniSem/Base/BaseException/TypeCastException.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable UnusedType.Global
+
+using System;
+
+namespace miniSem.Base.BaseException {
+    public class TypeCastException : Exception {
+        public TypeCastException(Type type) : base($"转换到类型 {type} 错误!!") {
+            
+        }
+    }
+}

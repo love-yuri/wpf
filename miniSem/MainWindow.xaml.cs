de38f263d2ede9ef96e5a93045764ed1cde4b65e
love-yuri <yuri2078170658@gmail.com>
2025-01-10 15:31:39 +0800

    feat(MainWindow): 增加配置日志和改进滑块控件

--- miniSem/MainWindow.xaml.cs
+++ miniSem/MainWindow.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace miniSem {
+﻿using miniSem.Base;
+using miniSem.Base.Config;
+
+namespace miniSem {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
+            
+            var config = GlobalConfig.Instance;
+            var version = config.Version;
+            Log.Info($"name -> {config.Name} version -> {version}");
         }
     }
 }

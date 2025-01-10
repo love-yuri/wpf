971934dfa2fba25a45bb2549282da0dc127c00b7
love-yuri <yuri2078170658@gmail.com>
2025-01-10 15:30:51 +0800

    feat(Base/Config): 添加GlobalConfig类以管理全局配置

---
+++ miniSem/Base/Config/GlobalConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace miniSem.Base.Config {
+    
+    internal class GlobalConfig: BaseConfig {
+        protected override string BasePath => "config/global.json";
+        
+        private static GlobalConfig _instance;
+        public static GlobalConfig Instance => _instance ?? (_instance = Load<GlobalConfig>());
+
+        public string Name {
+            get => GetValue("yuri");
+            set => SetValue(value);
+        }
+
+        public string Version {
+            get => GetValue("1.0.0");
+            set => SetValue(value);
+        }
+    }
+}

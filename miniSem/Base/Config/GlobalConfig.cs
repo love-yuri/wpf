5463d3f0c2ab8651fbe5169c371cc1e28ddf4c2b
love-yuri <yuri2078170658@gmail.com>
2025-01-13 16:55:42 +0800

    feat(GlobalConfig): 添加Arrays属性并优化实例获取

--- miniSem/Base/Config/GlobalConfig.cs
+++ miniSem/Base/Config/GlobalConfig.cs
@@ -1,10 +1,13 @@
-﻿namespace miniSem.Base.Config {
+﻿using System.Collections.Generic;
+
+namespace miniSem.Base.Config {
     
     internal class GlobalConfig: BaseConfig {
         protected override string BasePath => "config/global.json";
         
-        private static GlobalConfig _instance;
+        // 全局共享实例
         public static GlobalConfig Instance => _instance ?? (_instance = Load<GlobalConfig>());
+        private static GlobalConfig _instance;
 
         public string Name {
             get => GetValue("yuri");
@@ -13,6 +16,11 @@
 
         public string Version {
             get => GetValue("1.0.0");
+            set => SetValue(value);
+        }
+
+        public List<string> Arrays {
+            get => GetValue(new List<string>());
             set => SetValue(value);
         }
     }

311de677ffa98ef150305bc0e14907f0cf5f7f83
love-yuri <yuri2078170658@gmail.com>
2025-01-16 16:57:35 +0800

    feat(MainWindow): 添加点击命令和炫彩按钮

--- miniSem/MainWindow.xaml.cs
+++ miniSem/MainWindow.xaml.cs
@@ -1,12 +1,23 @@
-﻿using miniSem.Base;
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using miniSem.Base;
 using miniSem.Base.Config;
+using miniSem.Base.Mvvm;
+using miniSem.Base.Utils;
+using miniSem.Components;
 
 namespace miniSem {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
+        public ICommand ClickCommand { get; }
+        
         public MainWindow() {
+            ClickCommand = BaseCommand.Create(() => {
+                BaseDialog.ShowDialog<TestDialog>(this);
+            });
+            
             InitializeComponent();
             
             var config = GlobalConfig.Instance;
@@ -19,6 +30,8 @@ namespace miniSem {
                 Log.Info($"v -> {k}");
             });
             config.Save();
+
+            Log.Info($"path -> {FileUtils.CurrentPath}");
         }
     }
 }

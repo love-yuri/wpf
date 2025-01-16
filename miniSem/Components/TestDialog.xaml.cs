f550c22ca091b343844cde423b3c506d1498cf94
love-yuri <yuri2078170658@gmail.com>
2025-01-16 16:56:08 +0800

    feat(Components): 添加测试Dialog

---
+++ miniSem/Components/TestDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace miniSem.Components {
+    public partial class TestDialog {
+        public TestDialog() {
+            InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}

3ea846c78058aa329e39dc149ece312bf0b20d54
love-yuri <yuri2078170658@gmail.com>
2025-01-16 16:56:45 +0800

    feat(Mvvm): 添加BaseCommand类以支持命令绑定

---
+++ miniSem/Base/Mvvm/BaseCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace miniSem.Base.Mvvm {
+    public class BaseCommand: ICommand {
+        public event EventHandler CanExecuteChanged;
+        private readonly Action _action;
+        private bool _canExecute;
+        
+        public void SetCanExecute(bool canExecute) {
+            _canExecute = canExecute;
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private BaseCommand(Action action, bool canExecute = true) {
+            _action = action;
+            _canExecute = canExecute;
+        }
+        
+        /// <summary>
+        /// 快速创建一个指令
+        /// </summary>
+        /// <param name="action">执行的任务</param>
+        /// <param name="canExecute">是否可以执行</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static BaseCommand Create(Action action, bool canExecute = true) {
+            if (action == null) {
+                throw new ArgumentNullException(nameof(action));
+            }
+            return new BaseCommand(action, canExecute);
+        }
+
+        public bool CanExecute(object parameter) => _canExecute;
+        public void Execute(object parameter) => _action?.Invoke(); 
+    }
+}

1ea2280ea504f98ced126187d794f445e61020df
love-yuri <yuri2078170658@gmail.com>
2025-01-16 16:55:48 +0800

    feat(FileUtils): 添加文件工具类及获取当前路径功能

---
+++ miniSem/Base/Utils/FileUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace miniSem.Base.Utils {
+    
+    
+    /// <summary>
+    /// 存储文件相关工具
+    /// </summary>
+    public static class FileUtils {
+
+        /// <summary>
+        /// 获取当前工作目录完全限定路径
+        /// </summary>
+        public static string CurrentPath => Environment.CurrentDirectory;
+
+        public static void OpenDirectory() {
+            Log.Info($"打开当前目录 -> {CurrentPath}");
+            Process.Start(CurrentPath);
+        }
+    }
+}

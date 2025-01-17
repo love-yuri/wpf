4014aa3e860850f56002c57bae220ca5bfb25966
love-yuri <yuri2078170658@gmail.com>
2025-01-17 17:03:06 +0800

    refactor: 移除冗余命名空间引用

--- miniSem/Base/Config/GlobalConfig.cs
+++ miniSem/Base/Config/GlobalConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Documents;
-using Newtonsoft.Json;
+﻿using System.Collections.Generic;
 
 namespace miniSem.Base.Config {
     
--- miniSem/Components/TestDialog.xaml.cs
+++ miniSem/Components/TestDialog.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Media.Animation;
+﻿using System.Windows;
 
 namespace miniSem.Components {
     public partial class TestDialog {

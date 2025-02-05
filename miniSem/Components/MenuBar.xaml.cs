9ca2472a81337e0db4aa329eef74935d5a007e91
love-yuri <yuri2078170658@gmail.com>
2025-02-05 16:10:34 +0800

    fix(MenuBar): 优化鼠标拖动逻辑并调整背景色

--- miniSem/Components/MenuBar.xaml.cs
+++ miniSem/Components/MenuBar.xaml.cs
@@ -7,7 +7,7 @@ namespace miniSem.Components {
     public delegate void MenubarClickEvent();
     
     public partial class MenuBar {
-        public MenubarClickEvent ImportImgClick = null;
+        private bool _canDragMove;
         private LanguageType _languageType = LanguageType.Chinese;
         private ThemeType _themeType = ThemeType.Light;
         public BaseCommand OpenCurrentPathCommand { get; } = BaseCommand.Create(FileUtils.OpenDirectory);
@@ -16,12 +16,32 @@ namespace miniSem.Components {
             InitializeComponent();
         }
 
-        protected override void OnMouseMove(MouseEventArgs e) {
-            // 判断鼠标是否按下
-            if (e.LeftButton == MouseButtonState.Pressed) {
-                CommonUtils.CurrentWindow.DragMove();
-            }
+        #region 鼠标移动/拖动事件
+
+        // 使用更深层次的信号传递
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e) {
+            _canDragMove = true;
         }
+
+        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e) {
+            _canDragMove = false;
+        }
+        
+        protected override void OnMouseMove(MouseEventArgs e) {
+            // 判断鼠标左键是否按下
+            if (e.LeftButton != MouseButtonState.Pressed || !_canDragMove) {
+                return;
+            }
+            if (CommonUtils.CurrentWindow.WindowState == WindowState.Maximized) {
+                CommonUtils.CurrentWindow.WindowState = WindowState.Normal;
+            }
+            CommonUtils.CurrentWindow.DragMove();
+        }
+
+        #endregion
+
+
+        #region 窗口工具
 
         private void CloseWindow(object sender, RoutedEventArgs e) {
             CommonUtils.CurrentWindow.Close();
@@ -30,6 +50,17 @@ namespace miniSem.Components {
         private void MiniWindow(object sender, RoutedEventArgs e) {
             CommonUtils.CurrentWindow.WindowState = WindowState.Minimized;
         }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e) {
+            if (e.ChangedButton != MouseButton.Left) {
+                return;
+            }
+            var newState = CommonUtils.CurrentWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            CommonUtils.CurrentWindow.WindowState = newState;
+
+        }
+
+        #endregion
 
         /// <summary>
         /// 切换语言

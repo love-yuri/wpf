44ef26dd565e831ae187792a75a984d2bbc153f8
love-yuri <yuri2078170658@gmail.com>
2025-02-07 16:42:38 +0800

    feat(Navigation): 添加导航服务和主页视图

---
+++ Wpf-net9/Navigation/NavigationService.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wpf_net9.Navigation;
+
+public class NavigationService(IServiceProvider serviceProvider) {
+    private Frame? _frame;
+
+    /**
+     * 导航到指定页面
+     */
+    public void GoTo(Type type) {
+        var page = serviceProvider.GetRequiredService(type);
+        _frame?.Navigate(page);
+    }
+
+    /**
+     * 设置frame
+     */
+    public void SetFrame(Frame frame) {
+        _frame = frame;
+    }
+}
---
+++ Wpf-net9/Views/HomePage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace Wpf_net9.Views;
+
+public partial class HomePage {
+    public HomePage() {
+        InitializeComponent();
+    }
+}
+
